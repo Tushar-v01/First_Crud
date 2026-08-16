61ff10fd943ab738185b8e9d688f7bab85fa45e9
Tushar Verma <tusharverma@Tushars-MacBook-Air-3.local>
2026-08-16 15:10:06 +0530

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json())
 
+const tasks = [
+  { id: 1, title: 'Attend class', done: false },
+  { id: 2, title: 'Complete A crud application',   done: true  },
+  { id: 3, title: 'Go for a walk', done: false },
+]
 
 app.get('/',(req,res)=>{
   res.json({
@@ -10,6 +16,19 @@ app.get('/',(req,res)=>{
     version:"1.0",
     endpoints:["/tasks"]
   })
+})
+app.get("/task",(req,res)=>{
+  res.send(tasks)
+})
+
+app.get("/task/:id",(req,res)=>{
+  const id=Number(req.params.id)
+  const task=tasks.find(item=>item.id===id)
+  if(!task)
+  {
+    return res.status(404).json({ error: `Task ${id} not found`})
+  }
+  res.json(task)
 })
 
 app.get('/health',(req,res)=>{
